9577dc841dcec7d3ecaa9c899e573bab0a21d631
Michael Wagner <mitch.wagna@gmail.com>
2017-04-11 22:04:35 +0200

    feat(object): implement `require` as Kernel function

--- object/kernel.go
+++ object/kernel.go
@@ -2,6 +2,13 @@ package object
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/goruby/goruby/lexer"
+	"github.com/goruby/goruby/parser"
 )
 
 var kernelModule = newModule("Kernel", kernelMethodSet)
@@ -76,5 +83,45 @@ func kernelRequire(context CallContext, args ...RubyObject) (RubyObject, error) 
 	if !ok {
 		return nil, NewImplicitConversionTypeError(name, args[0])
 	}
-	return &RequireStatement{name}, nil
+	filename := name.Value
+	if !strings.HasSuffix(filename, "rb") {
+		filename += ".rb"
+	}
+	absolutePath, _ := filepath.Abs(filename)
+	loadedFeatures, ok := context.Env().Get("$LOADED_FEATURES")
+	if !ok {
+		loadedFeatures = NewArray()
+		context.Env().SetGlobal("$LOADED_FEATURES", loadedFeatures)
+	}
+	arr, ok := loadedFeatures.(*Array)
+	if !ok {
+		arr = NewArray()
+	}
+	loaded := false
+	for _, feat := range arr.Elements {
+		if feat.Inspect() == absolutePath {
+			loaded = true
+			break
+		}
+	}
+	if loaded {
+		return FALSE, nil
+	}
+
+	file, err := ioutil.ReadFile(filename)
+	if os.IsNotExist(err) {
+		return nil, NewLoadError(name.Value)
+	}
+	l := lexer.New(string(file))
+	p := parser.New(l)
+	prog, err := p.ParseProgram()
+	if err != nil {
+		return nil, NewSyntaxError(err.Error())
+	}
+	_, err = context.Eval(prog, context.Env())
+	if err != nil {
+		return nil, err
+	}
+	arr.Elements = append(arr.Elements, &String{Value: absolutePath})
+	return TRUE, nil
 }
--- object/kernel_test.go
+++ object/kernel_test.go
@@ -1,9 +1,12 @@
 package object
 
 import (
+	"path/filepath"
 	"reflect"
 	"sort"
 	"testing"
+
+	"github.com/goruby/goruby/ast"
 )
 
 func TestKernelMethods(t *testing.T) {
@@ -261,27 +264,364 @@ func TestKernelClass(t *testing.T) {
 }
 
 func TestKernelRequire(t *testing.T) {
-	context := &callContext{receiver: NIL}
-	name := &String{"foo"}
+	t.Run("wiring together", func(t *testing.T) {
+		evalCallCount := 0
+		var evalCallASTNode ast.Node
+		var evalCallEnv Environment
+		eval := func(node ast.Node, env Environment) (RubyObject, error) {
+			evalCallCount++
+			evalCallASTNode = node
+			evalCallEnv = env
+			return TRUE, nil
+		}
 
-	result, err := kernelRequire(context, name)
+		context := &callContext{
+			env:      NewEnvironment(),
+			eval:     eval,
+			receiver: &Object{},
+		}
+		name := &String{"./fixtures/testfile.rb"}
 
-	if err != nil {
-		t.Logf("expected no error, got %T:%v\n", err, err)
-		t.Fail()
-	}
+		result, err := kernelRequire(context, name)
 
-	req, ok := result.(*RequireStatement)
-	if !ok {
-		t.Logf("Expected RequireStatement, got %#v", result)
-		t.FailNow()
-	}
+		if err != nil {
+			t.Logf("expected no error, got %T:%v\n", err, err)
+			t.Fail()
+		}
 
-	actual := req.Name.Value
-	expected := "foo"
+		boolean, ok := result.(*Boolean)
+		if !ok {
+			t.Logf("Expected Boolean, got %#v", result)
+			t.FailNow()
+		}
 
-	if actual != expected {
-		t.Logf("Expected require name to equal %s, got %s", expected, actual)
-		t.Fail()
-	}
+		if boolean != TRUE {
+			t.Logf("Expected return to equal TRUE, got FALSE")
+			t.Fail()
+		}
+
+		if evalCallCount != 1 {
+			t.Logf("Expected context.Eval to be called once, was %d\n", evalCallCount)
+			t.Fail()
+		}
+
+		expectedASTNodeString := "x = 5"
+		actualASTNodeString := evalCallASTNode.String()
+		if expectedASTNodeString != actualASTNodeString {
+			t.Logf("Expected Eval AST param to equal %q, got %q\n", expectedASTNodeString, actualASTNodeString)
+			t.Fail()
+		}
+	})
+	t.Run("env side effects no $LOADED_FEATURES", func(t *testing.T) {
+		env := NewEnvironment()
+		eval := func(node ast.Node, env Environment) (RubyObject, error) {
+			return TRUE, nil
+		}
+
+		context := &callContext{
+			env:      env,
+			eval:     eval,
+			receiver: &Object{},
+		}
+		name := &String{"./fixtures/testfile.rb"}
+
+		_, err := kernelRequire(context, name)
+		if err != nil {
+			panic(err)
+		}
+
+		loadedFeatures, ok := env.Get("$LOADED_FEATURES")
+
+		if !ok {
+			t.Logf("Expected env to contain global $LOADED_FEATURES")
+			t.Fail()
+		}
+
+		arr, ok := loadedFeatures.(*Array)
+
+		if !ok {
+			t.Logf("Expected $LOADED_FEATURES to be an Array, got %T", loadedFeatures)
+			t.FailNow()
+		}
+
+		abs, _ := filepath.Abs("./fixtures/testfile.rb")
+		expected := NewArray(&String{abs})
+
+		if !reflect.DeepEqual(expected, arr) {
+			t.Logf("Expected $LOADED_FEATURES to equal\n%#v\n\tgot\n%#v\n", expected.Inspect(), arr.Inspect())
+			t.Fail()
+		}
+	})
+	t.Run("env side effects missing suffix", func(t *testing.T) {
+		env := NewEnvironment()
+		eval := func(node ast.Node, env Environment) (RubyObject, error) {
+			return TRUE, nil
+		}
+
+		context := &callContext{
+			env:      env,
+			eval:     eval,
+			receiver: &Object{},
+		}
+		name := &String{"./fixtures/testfile"}
+
+		_, err := kernelRequire(context, name)
+		if err != nil {
+			panic(err)
+		}
+
+		loadedFeatures, ok := env.Get("$LOADED_FEATURES")
+
+		if !ok {
+			t.Logf("Expected env to contain global $LOADED_FEATURES")
+			t.Fail()
+		}
+
+		arr, ok := loadedFeatures.(*Array)
+
+		if !ok {
+			t.Logf("Expected $LOADED_FEATURES to be an Array, got %T", loadedFeatures)
+			t.FailNow()
+		}
+
+		abs, _ := filepath.Abs("./fixtures/testfile.rb")
+		expected := NewArray(&String{abs})
+
+		if !reflect.DeepEqual(expected, arr) {
+			t.Logf("Expected $LOADED_FEATURES to equal\n%#v\n\tgot\n%#v\n", expected.Inspect(), arr.Inspect())
+			t.Fail()
+		}
+	})
+	t.Run("env side effects $LOADED_FEATURES exist", func(t *testing.T) {
+		env := NewEnvironment()
+		env.SetGlobal("$LOADED_FEATURES", NewArray(&String{"foo"}))
+		eval := func(node ast.Node, env Environment) (RubyObject, error) {
+			return TRUE, nil
+		}
+
+		context := &callContext{
+			env:      env,
+			eval:     eval,
+			receiver: &Object{},
+		}
+		name := &String{"./fixtures/testfile"}
+
+		_, err := kernelRequire(context, name)
+		if err != nil {
+			panic(err)
+		}
+
+		loadedFeatures, ok := env.Get("$LOADED_FEATURES")
+
+		if !ok {
+			t.Logf("Expected env to contain global $LOADED_FEATURES")
+			t.Fail()
+		}
+
+		arr, ok := loadedFeatures.(*Array)
+
+		if !ok {
+			t.Logf("Expected $LOADED_FEATURES to be an Array, got %T", loadedFeatures)
+			t.FailNow()
+		}
+
+		abs, _ := filepath.Abs("./fixtures/testfile.rb")
+		expected := NewArray(&String{"foo"}, &String{abs})
+
+		if !reflect.DeepEqual(expected, arr) {
+			t.Logf("Expected $LOADED_FEATURES to equal\n%#v\n\tgot\n%#v\n", expected.Inspect(), arr.Inspect())
+			t.Fail()
+		}
+	})
+	t.Run("file does not exist", func(t *testing.T) {
+		env := NewEnvironment()
+		eval := func(node ast.Node, env Environment) (RubyObject, error) {
+			return TRUE, nil
+		}
+
+		context := &callContext{
+			env:      env,
+			eval:     eval,
+			receiver: &Object{},
+		}
+		name := &String{"file/not/exist"}
+
+		_, err := kernelRequire(context, name)
+		if err == nil {
+			t.Logf("Expected error not to be nil")
+			t.Fail()
+		}
+
+		expectedErr := NewLoadError("file/not/exist")
+		if !reflect.DeepEqual(expectedErr, err) {
+			t.Logf("Expected error to equal %v, got %v", expectedErr, err)
+			t.Fail()
+		}
+
+		loadedFeatures, ok := env.Get("$LOADED_FEATURES")
+
+		if !ok {
+			t.Logf("Expected env to contain global $LOADED_FEATURES")
+			t.Fail()
+		}
+
+		arr, ok := loadedFeatures.(*Array)
+
+		if !ok {
+			t.Logf("Expected $LOADED_FEATURES to be an Array, got %T", loadedFeatures)
+			t.FailNow()
+		}
+
+		expected := NewArray()
+
+		if !reflect.DeepEqual(expected, arr) {
+			t.Logf("Expected $LOADED_FEATURES to equal\n%#v\n\tgot\n%#v\n", expected.Inspect(), arr.Inspect())
+			t.Fail()
+		}
+	})
+	t.Run("syntax error", func(t *testing.T) {
+		env := NewEnvironment()
+		eval := func(node ast.Node, env Environment) (RubyObject, error) {
+			return TRUE, nil
+		}
+
+		context := &callContext{
+			env:      env,
+			eval:     eval,
+			receiver: &Object{},
+		}
+		name := &String{"./fixtures/testfile_syntax_error.rb"}
+
+		_, err := kernelRequire(context, name)
+		if err == nil {
+			t.Logf("Expected error not to be nil")
+			t.Fail()
+		}
+
+		expectedErr := NewSyntaxError("Parsing errors:\n\texpected next token to be of type [END], got EOF instead\n")
+		if !reflect.DeepEqual(expectedErr, err) {
+			t.Logf("Expected error to equal\n%q\n\tgot\n%q", expectedErr, err)
+			t.Fail()
+		}
+
+		loadedFeatures, ok := env.Get("$LOADED_FEATURES")
+
+		if !ok {
+			t.Logf("Expected env to contain global $LOADED_FEATURES")
+			t.Fail()
+		}
+
+		arr, ok := loadedFeatures.(*Array)
+
+		if !ok {
+			t.Logf("Expected $LOADED_FEATURES to be an Array, got %T", loadedFeatures)
+			t.FailNow()
+		}
+
+		expected := NewArray()
+
+		if !reflect.DeepEqual(expected, arr) {
+			t.Logf("Expected $LOADED_FEATURES to equal\n%#v\n\tgot\n%#v\n", expected.Inspect(), arr.Inspect())
+			t.Fail()
+		}
+	})
+	t.Run("thrown error", func(t *testing.T) {
+		env := NewEnvironment()
+		eval := func(node ast.Node, env Environment) (RubyObject, error) {
+			return nil, NewException("something went wrong")
+		}
+
+		context := &callContext{
+			env:      env,
+			eval:     eval,
+			receiver: &Object{},
+		}
+		name := &String{"./fixtures/testfile_name_error.rb"}
+
+		_, err := kernelRequire(context, name)
+		if err == nil {
+			t.Logf("Expected error not to be nil")
+			t.Fail()
+		}
+
+		expectedErr := NewException("something went wrong")
+		if !reflect.DeepEqual(expectedErr, err) {
+			t.Logf("Expected error to equal\n%q\n\tgot\n%q", expectedErr, err)
+			t.Fail()
+		}
+
+		loadedFeatures, ok := env.Get("$LOADED_FEATURES")
+
+		if !ok {
+			t.Logf("Expected env to contain global $LOADED_FEATURES")
+			t.Fail()
+		}
+
+		arr, ok := loadedFeatures.(*Array)
+
+		if !ok {
+			t.Logf("Expected $LOADED_FEATURES to be an Array, got %T", loadedFeatures)
+			t.FailNow()
+		}
+
+		expected := NewArray()
+
+		if !reflect.DeepEqual(expected, arr) {
+			t.Logf("Expected $LOADED_FEATURES to equal\n%#v\n\tgot\n%#v\n", expected.Inspect(), arr.Inspect())
+			t.Fail()
+		}
+	})
+	t.Run("already loaded", func(t *testing.T) {
+		abs, _ := filepath.Abs("./fixtures/testfile.rb")
+		env := NewEnvironment()
+		env.SetGlobal("$LOADED_FEATURES", NewArray(&String{abs}))
+		eval := func(node ast.Node, env Environment) (RubyObject, error) {
+			return TRUE, nil
+		}
+
+		context := &callContext{
+			env:      env,
+			eval:     eval,
+			receiver: &Object{},
+		}
+		name := &String{"./fixtures/testfile.rb"}
+
+		result, err := kernelRequire(context, name)
+		if err != nil {
+			t.Logf("Expected no error, got %T:%v", err, err)
+			t.Fail()
+		}
+
+		boolean, ok := result.(*Boolean)
+		if !ok {
+			t.Logf("Expected Boolean, got %#v", result)
+			t.FailNow()
+		}
+
+		if boolean != FALSE {
+			t.Logf("Expected return to equal FALSE, got TRUE")
+			t.Fail()
+		}
+
+		loadedFeatures, ok := env.Get("$LOADED_FEATURES")
+
+		if !ok {
+			t.Logf("Expected env to contain global $LOADED_FEATURES")
+			t.Fail()
+		}
+
+		arr, ok := loadedFeatures.(*Array)
+
+		if !ok {
+			t.Logf("Expected $LOADED_FEATURES to be an Array, got %T", loadedFeatures)
+			t.FailNow()
+		}
+
+		expected := NewArray(&String{abs})
+
+		if !reflect.DeepEqual(expected, arr) {
+			t.Logf("Expected $LOADED_FEATURES to equal\n%#v\n\tgot\n%#v\n", expected.Inspect(), arr.Inspect())
+			t.Fail()
+		}
+	})
 }
